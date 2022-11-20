0402d2464955dadb2439535e7a73f86079dee8a1
DanielMariscal <mariscaldaniel@gmail.com>
2022-11-20 08:15:52 -0400

    2 commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,21 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+   return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length-1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 
@@ -23,6 +26,10 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  for(let i=0;i<array.length;i++){
+       array[i]=array[i]+1;
+  }
+  return array;
 }
 
 
@@ -30,6 +37,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array;
 }
 
 
@@ -38,6 +47,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+     array.unshift(elemento);
+     return array;
 }
 
 
@@ -47,6 +58,21 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  let res="";
+  if(palabras.length>1){
+  for(let i=0;i<palabras.length;i++){
+    if(i==0){
+      res=palabras[i];
+    }
+    else{
+    res =res+" "+palabras[i];
+    }
+  }
+}
+else if(palabras.length==1){
+  res=palabras[0];   
+}
+  return res;
 }
 
 
@@ -54,6 +80,13 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  let res=false;
+  for(let i=0;i<array.length;i++){
+      if(array[i]===elemento){
+          res=true;
+      }
+  }
+  return res;
 }
 
 
@@ -61,6 +94,11 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  let res=0;
+  for(let i=0;i<numeros.length;i++){
+       res=res+numeros[i];
+  }
+  return res;
 }
 
 
@@ -68,6 +106,12 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  let res=0;
+  for(let i=0;i<resultadosTest.length;i++){
+       res=res+resultadosTest[i];
+  }
+  res=res/resultadosTest.length;
+  return res;
 }
 
 
@@ -75,6 +119,13 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  let res=-9999;
+  for(let i=0;i<numeros.length;i++){
+    if(res<numeros[i]){
+      res=numeros[i];
+    }
+  }
+  return res;
 }
 
 
@@ -82,12 +133,29 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if(arguments.length>0){
+     let res=1;
+     for(let i=0;i<arguments.length;i++){
+        res=res*arguments[i];
+     }
+     return res;
+  }
+  else{
+    return 0;
+  }
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  let res=0;
+  for(let i=0;i<arreglo.length;i++){
+     if(arreglo[i]>18){
+      res=res+1;
+     }
+  }
+  return res;
 
 }
 
@@ -97,6 +165,12 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
+  if(numeroDeDia===1||numeroDeDia===7){
+    return "Es fin de semana";
+  }
+  else{
+    return "Es dia Laboral";
+  }
   
 } 
 
@@ -105,6 +179,28 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  var res2=false;
+  let res=n;
+  if(n<10 && n!=9){
+    res2=false;
+  }
+  else if(n===9){
+    res2=true;
+  }
+  else{
+     for(let i=0;res>1;i++){
+        res=res/10;
+        console.log(res);
+        let m=false;
+        if(res>9 && res<10){
+            m=true;
+        }
+        if(m===true){
+            res2=true;
+        }
+     }
+  }
+  return res2;
   
 }
 
@@ -112,7 +208,15 @@ function empiezaConNueve(n) {
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí  
+  //Escribe tu código aquí
+  let res2=true;  
+  let res=arreglo[0];
+  for(let i=0;i<arreglo.length;i++){
+       if(res!=arreglo[i]){
+        res2=false;
+       }
+  }
+  return res2;
   
 } 
 
@@ -122,6 +226,17 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  let res="No se encontraron los meses pedidos";
+  let cont=[];
+  for(let i=0;i<array.length;i++){
+     if(array[i]==="Enero" || array[i]==="Marzo" || array[i]==="Noviembre"){
+         cont.push(array[i]);
+     }
+  }
+  if(cont.length===3){
+    res=cont;
+  }
+  return res;
 }
 
 
@@ -129,6 +244,13 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  let res=[];
+  for(let i=0;i<array.length;i++){
+     if(array[i]>100){
+       res.push(array[i]);
+     }
+  }
+  return res;
 }
 
 
@@ -139,7 +261,20 @@ function breakStatement(numero) {
   //Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, debe interrumpirse la ejecución y 
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
+  let num=numero;
   // Tu código:
+  let res=[];
+  for(let i=0;i<10;i++){
+     num=num+2;
+     if(num===10){
+       return "Se interrumpió la ejecución";
+       break;
+     }
+     else{
+      res.push(num);
+     }
+  } 
+  return res;
 }
 
 
@@ -150,6 +285,20 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  let num=numero;
+  // Tu código:
+  let res=[];
+  for(let i=0;i<10;i++){
+     num=num+2;
+     if(i===4){
+       num=num-2;
+       continue;
+     }
+     else{
+      res.push(num);
+     }
+  } 
+  return res;
 }
 
 
--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,14 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  const gato={
+    nombre:nombre,
+    edad:edad,
+    meow:function(){
+      return "Meow!";
+    }
+  };
+  return gato;
 }
 
 
@@ -14,6 +22,9 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto [property]=null;
+  return objeto;
+
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -21,12 +32,15 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+    m=metodo+'()';
+    objeto.incrementX();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
+     return objetoMisterioso.numeroMisterioso*5;
 
 }
 
@@ -35,12 +49,20 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+   delete objeto[unaPropiedad];
+   return objeto;
 }
 
 function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
+  const usuario={
+    nombre:nombre,
+    email:email,
+    password:password
+  };
+  return usuario;
 
 }
 
@@ -48,21 +70,36 @@ function tieneEmail(usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  let res=false;
+  if(usuario.hasOwnProperty("email")){
+    if(usuario.email!=null){
+        res=true;
+    }
+  }
+  return res;
 }
 
 
 
-function tienePropiedad(objeto, propiedad) {
+function tienePropiedad(usuario, propiedad) {
   // Devuelve "true" si el objeto (parámetro "objeto") tiene una propiedad (key) cuyo nombre es igual al valor del argumento "propiedad"
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  let res=false;
+  if(usuario.hasOwnProperty(propiedad)){
+    if(usuario[propiedad]!=null){
+        res=true;
+    }
+  }
+  return res;
 }
 
 function verificarPassword(usuario, password) {
   // Comprueba si la "password" enviada coincide con la propiedad "password" del objeto "usuario"
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
+  return usuario.password===password;
   // // Tu código:
 }
 
@@ -70,12 +107,16 @@ function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password=nuevaPassword;
+  return usuario;
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
   // "usuario" tiene una propiedad llamada "amigos" que es un array
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
+  usuario.amigos.push(nuevoAmigo);
+  return usuario;
   // // Tu código:
 }
 
@@ -85,8 +126,11 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+      for (let i = 0; i< usuarios.length; i++) {
+               usuarios[i].esPremium=true;
+      }
+      return usuarios;
 }
-
 function sumarLikesDeUsuario(usuario) {
   // "usuario" tiene una propiedad llamada "posts" que es un array
   // "posts" es un array de objetos "post"
@@ -94,6 +138,12 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  let contador=0;
+  arrayPots=usuario.posts;
+  for (let i = 0; i < arrayPots.length; i++) {
+       contador=contador+arrayPots[i].likes;
+  }
+  return contador;
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -106,7 +156,11 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+    producto.calcularPrecioDescuento=function(){
+      let descuento= producto.precio*producto.porcentajeDeDescuento;
+      return producto.precio-descuento;
+    }
+    return producto;
 }
 
 // No modificar nada debajo de esta línea
